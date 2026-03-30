fa80bf14c536bef0c1c5c222b1a0e8425ee98032
raiton-boo <raito.build@gmail.com>
2026-03-30 23:08:35 +0900

    chore: 設定の変更

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  distDir: 'docs',
   trailingSlash: true,
   reactCompiler: true,
   basePath: isProd ? '/year-project' : '',
